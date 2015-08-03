1bae68796323d043e4f877b191f11cace02581aa
ridaOunana <oviich1@gmail.com>
2015-08-03 11:17:17 +0100

    2nd commit.

---
+++ m2m_restaurant/src/main/java/ma/m2m/restau/M2mRestaurantApplication.java
@@ -0,0 +1,12 @@
+package ma.m2m.restau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2mRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M2mRestaurantApplication.class, args);
+    }
+}
